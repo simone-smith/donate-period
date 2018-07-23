f22cd31542899b8f4457e4863f16fd7a10c9df9e
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-23 18:09:41 +0100

    Add wishlist GET route
    * Returns JSON sample content

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = 3000;
+
+const wishlist = [
+                    { "item": "toilet paper" },
+                    { "item": "sanitary pads" },
+                    { "item": "tampons" },
+                    { "item": "wet wipes" }
+                 ];
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, () => console.log('App is listening on port 3000!'))
+
+app.get('/wishlist/', function(req, res) {
+    res.json(wishlist);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}!`)
+});
