4e85ed7ba5e0be1eaf9db68421debbbc4f518d1e
Simone <simonecaplin@gmail.com>
2018-07-24 12:37:49 +0100

    Set up React project and pass first test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class CheckboxWithLabel extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+ReactDOM.render(<CheckboxWithLabel />, document.getElementById('root'));
