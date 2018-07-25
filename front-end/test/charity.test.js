a3362af843ab19f323a43d283708468939bc6f6f
Jen Strong <jenniferastrong@gmail.com>
2018-07-25 15:23:54 +0100

    Create Charity component
    * Charity component displays button
    * Clicking the button displays the wishlist
    * Test for button name - passes

---
+++ front-end/test/charity.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Enzyme from 'enzyme';
+import {shallow} from 'enzyme';
+import Charity from '../src/charity.js'
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() })
+
+test("Charity displays a link on the page", () => {
+  const wrapper = shallow(<Charity />);
+  const input = wrapper.find('input');
+  expect(input.render().attr('value')).toEqual("Charity's Wishlist")
+});
+
+test("When you click the Charity link, it returns the wishlist", () => {
+
+});
