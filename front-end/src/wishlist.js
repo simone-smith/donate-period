4f6d4fb7790c2540b8e0949092051af90a3296f3
anguspettifer <angus.pettifer@gmail.com>
2018-07-25 12:56:47 +0100

    Create wishlist component
    * Pending test
    * Call to back-end API

--- front-end/src/wishlist.js
+++ front-end/src/wishlist.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
 export default class Wishlist extends React.Component {
+
+  constructor() {
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3010/wishlist/')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <h1>Hello</h1>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+        {items.map(item => (
+          <li key={item['item']}>
+            {item['item']}
+          </li>
+        ))}
+        </ul>
+      );
+    }
   }
 }
