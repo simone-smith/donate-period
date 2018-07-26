da5cbe7cc6b83a1d95c8e25be1bade146ecf93eb
Jen Strong <jenniferastrong@gmail.com>
2018-07-26 12:02:34 +0100

    Add Post Route for Wishlist Endpoint
    *Import bodyParser for parsing JSON
    *Add POST route which expects JSON
    *Add test to test request status and response body

--- back-end/test/app.js
+++ back-end/test/app.js
@@ -3,6 +3,7 @@ const app = require('../app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
@@ -18,4 +19,20 @@ describe('/GET wishlist', () => {
         done();
       });
   });
+});
+
+
+describe('/POST wishlist', () => {
+  it ('it should POST a new wishlist', (done) => {
+    chai.request(app)
+      .post('/wishlist')
+      .send({ "item" : "kleenex" })
+      .end(function (err, res){
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body[4]).to.have.property('item','kleenex');
+        done();
+      })
+  })
 });
