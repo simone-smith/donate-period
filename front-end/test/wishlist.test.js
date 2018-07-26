2251251c842e1fa865282079a030d5ad5bf76649
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-26 12:38:53 +0100

    Add test to display error for wislist
    * Unit test functionality

--- front-end/test/wishlist.test.js
+++ front-end/test/wishlist.test.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Enzyme from 'enzyme';
 import {shallow} from 'enzyme';
+
+import { mount } from 'enzyme';
+
 import Wishlist from '../src/wishlist.js'
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 import 'whatwg-fetch';
 
-test("Wishlist displays a wishlist on the page", () => {
-  const wishlist = shallow(<Wishlist />);
-  expect(wishlist.text()).toEqual("toilet paper, sanitary pads, tampons, wet wipes")
+describe('<Wishlist />', () => {
+
+  describe('Render', () => {
+
+    it("An error is displayed", async () => {
+        let wishlist = shallow(<Wishlist/>);
+
+        wishlist.setState({
+            error: true,
+            isLoaded: true,
+            items: []
+        });
+
+        wishlist.render(wishlist.state);
+
+        expect(wishlist.text()).toEqual("Error: ")
+    });
+
+  });
+
+
 });
