0e38f4018face48ea0bf14ae468b5e5179d92325
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-28 16:24:52 +0100

    Components stored in seperate file
    * Update for cleaner application structure

---
+++ front-end/src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Charity from './charity';
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <Charity />
+    );
+  }
+}
---
+++ front-end/src/components/wishlist.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class Wishlist extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3010/wishlist/')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+        {items.map(item => (
+          <li key={item['item']}>
+            {item['item']}
+          </li>
+        ))}
+        </ul>
+      );
+    }
+  }
+};
