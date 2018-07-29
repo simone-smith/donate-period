b8d6a7c58bf9f2c4201332cbbcabd9ca4e5ea3c5
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-29 22:03:52 +0100

    Update React dynamic routing
    * Move BrowserRouter to index.js to enable full dynamic routing
    * Fix issue with About being rendered below App component

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './components/app';
+import About from './components/about';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </BrowserRouter>,
+  document.getElementById('root')
+);
