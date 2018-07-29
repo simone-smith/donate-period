772f8b38cf7c10387184a2a686c1276dfe7b3879
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-29 10:36:21 +0100

    Add successful render test for frontend app

---
+++ front-end/test/app.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import App from '../src/components/app.js'
+
+describe('<App />', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div);
+  });
+
+});
