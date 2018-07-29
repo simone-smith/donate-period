debeee889d73aeb26d06fd87290ea0086528178f
Jen <35671443+JenStrong@users.noreply.github.com>
2018-07-29 14:34:02 +0100

    Update about.js

--- front-end/src/components/about.js
+++ front-end/src/components/about.js
@@ -7,7 +7,7 @@ class About extends Component {
       <div className="pageContainer">
         <h1>About Us</h1>
          <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
          </p>
          <p>Find out what is happening in the world of Donate Period:</p>
           <ul>
@@ -18,7 +18,7 @@ class About extends Component {
             </li>
             <li>
               <a href="https://github.com/simone-smith/donate-period">
-                What we're working on now
+                What we are working on now
               </a>
             </li>
           </ul>
