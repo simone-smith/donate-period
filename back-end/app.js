4b3bb8d065da4c971067841063cb8cf75e5e2d5d
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-30 18:05:00 +0100

    Test data structure for new wishlists

--- back-end/app.js
+++ back-end/app.js
@@ -7,14 +7,73 @@ const bodyParser = require('body-parser')
 app.use(cors());
 app.use(bodyParser.json());
 
-const wishlist = [
-                    { "item": "toilet paper" },
-                    { "item": "sanitary pads" },
-                    { "item": "tampons" },
-                    { "item": "wet wipes" }
-                 ];
+const charityWishlists = [ { "charityId": "2201",
+                              "wishlist": [
+                                { "itemID": 1,
+                                  "itemName": "toilet paper",
+                                  "itemDescription": "Great paper",
+                                  "itemImage": "http://images.justgiving.com/image/6ad95821-1a25-43ba-8f62-05216b3fd4fc.jpg",
+                                  "itemPrice": 2.99
+                                },
+                                { "itemID": 2,
+                                  "itemName": "tampons",
+                                  "itemDescription": "Great stuff",
+                                  "itemImage": "http://images.justgiving.com/image/b410179a-2042-4c6b-903b-df106b48fc3c.jpg",
+                                  "itemPrice": 1.99
+                                }
+                              ]
+                            },
+                            { "charityId": "2357",
+                                "wishlist": [
+                                  { "itemID": 3,
+                                    "itemName": "cleaning fluid",
+                                    "itemDescription": "Great fluid",
+                                    "itemImage": "http://images.justgiving.com/image/e5a09986-d0cd-4196-a0a6-4d7acb9b8ea4.png",
+                                    "itemPrice": 4.99
+                                  },
+                                  { "itemID": 2,
+                                    "itemName": "tampons",
+                                    "itemDescription": "Great stuff",
+                                    "itemImage": "http://images.justgiving.com/image/b410179a-2042-4c6b-903b-df106b48fc3c.jpg",
+                                    "itemPrice": 1.99
+                                  }
+                                ]
+                              },
+                              { "charityId": "2116",
+                                "wishlist": [
+                                  { "itemID": 1,
+                                    "itemName": "toilet paper",
+                                    "itemDescription": "Great paper",
+                                    "itemImage": "http://images.justgiving.com/image/6ad95821-1a25-43ba-8f62-05216b3fd4fc.jpg",
+                                    "itemPrice": 2.99
+                                  },
+                                  { "itemID": 2,
+                                    "itemName": "tampons",
+                                    "itemDescription": "Great stuff",
+                                    "itemImage": "http://images.justgiving.com/image/b410179a-2042-4c6b-903b-df106b48fc3c.jpg",
+                                    "itemPrice": 1.99
+                                  },
+                                  { "itemID": 3,
+                                    "itemName": "cleaning fluid",
+                                    "itemDescription": "Great fluid",
+                                    "itemImage": "http://images.justgiving.com/image/e5a09986-d0cd-4196-a0a6-4d7acb9b8ea4.png",
+                                    "itemPrice": 4.99
+                                  },
+                                ]
+                              }
+                          ];
 
-app.get('/wishlist/', function(req, res) {
+
+ // app.get('/charity/:charityId', function(req, res) {
+ //   const charityWishlist = charityWishlists.find(c => c.charityId === parseInt(req.params.charityId));
+ //   // const msg_404 = 'The wishlist for the given ID not found';
+ //   if (!charityWishlists) return res.status(404).send(msg_404);
+ //
+ //   res.send(charityWishlist);
+ // });
+
+
+app.get('/wishlist /', function(req, res) {
   res.json(wishlist);
 });
 
