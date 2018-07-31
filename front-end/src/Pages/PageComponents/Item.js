f227a52bff0eb977e619cf1cffc6886ea4694f5e
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-31 21:44:57 +0100

    Add Item component
    * Use wishlist to improve single responsiblity of wishlist
    component - take item  presentation logic from wishlist
    * Add checkbox as per site mock up
    * Add item counter with up and down button as per site mock up

---
+++ front-end/src/Pages/PageComponents/Item.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        checked: true,
+        count: 0
+      };
+    }
+
+  handleChange() {
+    this.setState({
+      checked: (this.state.checked === true ? false : true)
+    });
+  }
+
+  handleIncrease() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  handleDecrease() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  render() {
+    const { itemName, description, price, image } = this.props;
+    return (
+      <div>
+        <p>{itemName}</p>
+        <p>{description}</p>
+        <p>£{price}</p>
+        <button onClick={ () => this.handleIncrease() }>Up</button>
+        <p>{this.state.count}</p>
+        <button onClick={ () => this.handleDecrease() }>Down</button>
+        <br/>
+        <img src={image} height="42" width="auto" alt={itemName}></img>
+        <input type='checkbox' onChange={ () => this.handleChange() }/>
+        <div>{this.props.children}</div>
+      </div>
+    )
+  }
+};
+
+export default Item;
---
+++ front-end/test/Item.test.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import CharitiesBuilder from '../src/Pages/CharitiesBuilder.js'
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+import Item from '../src/Pages/PageComponents/Item';
+
+describe('<Item />', () => {
+
+  const globalProps = {
+    itemName: 'Test Name',
+    description: 'This is a test product description',
+    price: "2.99",
+    image: "http://images.testimage1.jpg"
+  };
+
+  let item = shallow(<Item {...globalProps}/>);
+
+  it('Renders the item name', () => {
+    const props = {
+      ...globalProps,
+    };
+
+    expect(item.find('p').at(0).text()).toEqual(props.itemName)
+  })
+
+  it('Renders the item decription', () => {
+    const props = {
+      ...globalProps,
+    };
+
+    expect(item.find('p').at(1).text()).toEqual(props.description)
+  })
+
+  it('Renders the item price', () => {
+    const props = {
+      ...globalProps,
+    };
+
+    expect(item.find('p').at(2).text()).toEqual("£" + props.price)
+  })
+
+  it('Renders the increase button', () => {
+    expect(item.find('button').at(0).text()).toEqual('Up')
+  })
+
+  it('Renders the decrease button', () => {
+    expect(item.find('button').at(1).text()).toEqual('Down')
+  })
+
+  it('Renders the image', () => {
+    const img = item.find('img');
+    expect(img.render().attr('src')).toEqual("http://images.testimage1.jpg")
+  })
+
+  it('Renders a control checkbox', () => {
+    expect(item.find('input').exists()).toBe(true);
+  })
+
+  describe('On clicking increase', () => {
+
+    beforeEach(() => {
+      item.find('button').at(0).simulate('click')
+    });
+
+    it('Increases the counter', () => {
+      expect(item.state().count).toEqual(1)
+      expect(item.find('p').at(3).text()).toEqual("1")
+    });
+
+  });
+
+  describe('On clicking increase', () => {
+
+    beforeEach(() => {
+      item.setState({ count: 1 })
+      item.find('button').at(1).simulate('click')
+    });
+
+    it('Decrease the counter', () => {
+      expect(item.state().count).toEqual(0)
+      expect(item.find('p').at(3).text()).toEqual("0")
+    });
+
+  });
+
+  describe('On clicking the check box', () => {
+
+    beforeEach(() => {
+      item.find('input').at(0).simulate('check', { target: { checked: true } })
+    });
+
+    it('Updates the state of checked', () => {
+      expect(item.state().checked).toEqual(true)
+    })
+
+  })
+
+})
