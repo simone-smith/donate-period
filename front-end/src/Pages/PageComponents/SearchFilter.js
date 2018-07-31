5a31feca50625e12c15f6b3259ab31e16c7ed8e2
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-31 14:29:49 +0100

    Add SearchFilter option
    * Component needs to take props of charities (so be rendered as a child
    of CharitiesBuilder).
    * Filters by comparing user input with the charities array
    * Then needs to pass the charity info to a Charity child component as
    props

---
+++ front-end/src/Pages/PageComponents/SearchFilter.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Charity from '../Charity.js';
+
+class SearchFilter extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      searchFilter: ""
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ searchFilter: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ searchFilter: event.target.value });
+  }
+
+  render() {
+    this.props.charities
+    let filteredCharities = this.props.charities.filter(
+      (charity) => {
+        return charity.name.indexOf(this.state.searchFilter) !== -1;
+      }
+    );
+
+    return (
+      <div>
+        <form className='search-filter' onSubmit={() => this.handleSubmit(event)}>
+          <input onChange={ event => this.handleChange(event) } />
+          <input type="submit" value="Search" />
+        </form>
+        <ul>
+          {filteredCharities.map((charity) => {
+            return <Charity charityName={charity} key={charity.charityId} />
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SearchFilter;
---
+++ front-end/test/SearchFilter.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() })
+import SearchFilter from '../src/Pages/PageComponents/SearchFilter';
+
+describe('<SearchFilter />', () => {
+  let searchFilter = shallow(<SearchFilter charities={ [{ name : "Alzheimer's Society" }] }/>)
+
+  describe('Renders the search filter', () => {
+
+    it('Creates a form component', () => {
+      expect(searchFilter.find('form').exists()).toBe(true);
+    });
+
+    it('Renders a search button', () => {
+      expect(searchFilter.find('input').at(1).render().attr('value')).toEqual('Search');
+    });
+
+  describe('When typing a search query', () => {
+    let charityName = 'Test Charity';
+
+    beforeEach(() => {
+      searchFilter.find('input').at(0).simulate('change', {
+        target: { value: charityName }
+      });
+    });
+
+    it('.handleChange updates the text in state', () => {
+      expect(searchFilter.state().searchFilter).toEqual(charityName)
+    });
+
+    })
+
+  })
+
+})
