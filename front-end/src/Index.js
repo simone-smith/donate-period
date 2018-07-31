d26b4374e3c52c24c6839e0b61bc8e15d9504ac0
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-31 05:23:41 +0100

    Refactor BrowseRouter history interaction
    * Move BrowseRouter to index to inject page
      contents as props.children into the page layout
    * Move links into PageLayout functional component
      to allow for common header links and title bar
    * Subsequent common page components can now be
      more modular and dry

--- front-end/src/Index.js
+++ front-end/src/Index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
+import PageLayout from './Pages/PageComponents/PageLayout';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+
+ReactDOM.render(
+
+    <BrowserRouter>
+        <PageLayout>
+            <Router />
+        </PageLayout>
+    </BrowserRouter>,
+
+    document.getElementById('root'));
--- front-end/src/Router.js
+++ front-end/src/Router.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import CharitiesBuilder from './Pages/CharitiesBuilder';
@@ -12,16 +11,14 @@ class Router extends Component {
 
     render() {
         return (
-            <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' component={Home} />
+                    <Route exact path='/' component={Home}  />
                     <Route path='/About' component={About} />
                     <Route path='/Charities' component={CharitiesBuilder} />
                     <Route path='/Wishlist' component={Wishlist} />
                     <Route path='/Donate' component={Donate} />
                     <Redirect from='*' to='/' />
                 </Switch>
-            </BrowserRouter>
         );
     }
 }
