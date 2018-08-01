32487e7f9e1bbd2e16097a17e1cd74f0437d7beb
Benjamin Tomkins <bentomkins@hotmail.com>
2018-08-01 13:59:48 +0100

    Refactor basic page structure into 3 row flex col
    * Easier to maintain and refactor

--- front-end/src/Pages/PageComponents/Navbar.js
+++ front-end/src/Pages/PageComponents/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -6,12 +7,11 @@ const Navbar = () => {
             <div className="d-flex justify-content-center row-hl">
                 <div className="p-4 item-hl">Flex Item</div>
                 <div className="p-4 item-hl">Flex Item</div>
-                <div className="p-4 item-hl">Flex Item</div>
-            </div>
-            <div className="d-flex justify-content-between row-hl">
-                <div className="p-4 item-hl">Flex Item</div>
-                <div className="p-4 item-hl">Flex Item</div>
-                <div className="p-4 item-hl">Flex Item</div>
+                <div className="p-4 item-hl">
+                    <Link to={'/Home'}>Home</Link>{" | "}
+                    <Link to={'/Charities'}>Charity List</Link>{" | "}
+                    <Link to={'/About'}>About Us</Link>
+                </div>
             </div>
         </div>
     );
--- front-end/src/Pages/PageComponents/PageLayout.js
+++ front-end/src/Pages/PageComponents/PageLayout.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+
+
 import Navbar from "./Navbar";
 
 const PageLayout = (props) => {
     return (
-        <div>
-        <Navbar/>
-            <p>
-                <Link to={'/Home'}>Home</Link>{" | "}
-                <Link to={'/Charities'}>Charity List</Link>{" | "}
-                <Link to={'/About'}>About Us</Link>
-            </p>
-            <div>{props.children}</div>
-            <p>lo</p>
-        </div>
+            <div className="d-flex flex-column row-hl">
+                <div className="p-4 item-hl"><Navbar/></div>
+                <div className="p-4 item-hl">{props.children}</div>
+                <div className="p-4 item-hl">footer here</div>
+            </div>
     );
 };
 
