a7c2837adce52b61c262dd3e0eee0cefc19fd1bc
Jen Strong <jenniferastrong@gmail.com>
2018-08-01 15:45:01 +0100

    Add text for home and about pages
    * Text is not yet formatted

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -5,7 +5,8 @@ class Home extends Component {
   render() {
     return (
         <div>
-            HOME PAGE CONTENT HERE
+          <p>Giving individuals the power to make impactful donations by connecting them with the items charities need most.</p></br>
+          <p>Charities often have certain needed items that are expensive to purchase and also aren’t items are typically donated. Donors want to make impactful donations, but often don’t know what items the charity need to operate. Donate, Period solves this problem from both ends by creating a platform to connect donors with the items charities need most.</p></br> 
             <br></br>
             <Link to={'/Charities'}>View our charity partners</Link>
             {" "}
