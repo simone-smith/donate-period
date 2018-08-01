abbef8f11b2021a190f537006fa911bdcd283203
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-02 00:23:04 +0100

    Remove unnecessary import file

--- front-end/src/Pages/CharitiesBuilder.js
+++ front-end/src/Pages/CharitiesBuilder.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Charity from './PageComponents/Charity';
 
 class CharitiesBuilder extends Component {
   constructor(props) {
@@ -31,16 +30,19 @@ class CharitiesBuilder extends Component {
     const { charities } = this.state;
     return (
       <div>
-      <div>
-      <ul>
-      {charities.map(charity => (
-        <Charity key={charity['charityId']} name={charity['name']} description={charity['description']} registrationNumber={charity['registrationNumber']} logoUrl={charity['logoUrl']} charityId={charity['charityId']} />
-      ))}
-      </ul>
-      </div>
-      <div>
-        <Link to={'/'}>Back</Link>
-      </div>
+        <div>
+          <ul>
+            {charities.map(charity => (
+              <Charity key={charity['charityId']} name={charity['name']}
+              description={charity['description']}
+              registrationNumber={charity['registrationNumber']}
+              logoUrl={charity['logoUrl']} charityId={charity['charityId']} />
+            ))}
+          </ul>
+        </div>
+        <div>
+          <Link to={'/'}>Back</Link>
+        </div>
       </div>
     );
   }
