f71008969a2b47ea3c344233c6a9cf64b54bc7f1
Jen Strong <jenniferastrong@gmail.com>
2018-08-02 18:43:24 +0100

    Update fonts on homepage

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -5,11 +5,11 @@ class Home extends Component {
   render() {
     return (
         <div>
-            <h1 className="p-4 display-4 text-center">Donate, Period</h1>
-            <p className="m-2 text-monospace text-center text-secondary">
+            <h1 className="p-4 display-3 text-center">Donate, Period</h1>
+            <h4 className="m-2 text-center text-secondary">
                 Giving individuals the power to make impactful donations by connecting them with the items charities need most.
-            </p>
-            <p className={"text-secondary"}>
+            </h4>
+            <p className="lead text-secondary">
                 <br/>
                 Charities often have certain needed items that are expensive to purchase and also aren’t items are typically donated. Donors want to make impactful donations, but often don’t know what items the charity need to operate. Donate, Period solves this problem from both ends by creating a platform to connect donors with the items charities need most.
             </p>
