3e3731a90f17591f578c047b2bdabe2b8a3c5ec3
Benjamin Tomkins <bentomkins@hotmail.com>
2018-08-02 16:17:32 +0100

    Add wishlist styling

--- front-end/src/Pages/PageComponents/PageLayout.js
+++ front-end/src/Pages/PageComponents/PageLayout.js
@@ -10,7 +10,7 @@ const PageLayout = (props) => {
             <div className="d-flex flex-column w-100 m-0 p-0">
                 <div className="mp-0 item-hl"> <Navbar/> </div>
                 <div className="p-4 item-hl">
-                    <Content padding="220"> {props.children} </Content>
+                    <Content padding="180"> {props.children} </Content>
                 </div>
                 <div className="mp-0 item-hl"> <Footer/> </div>
             </div>
