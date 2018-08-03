08f1804e5c02fda33951012bec444d7f6c616a95
Simone <simonecaplin@gmail.com>
2018-08-03 12:21:32 +0100

    Fix tests for Home component

--- front-end/test/Home.test.js
+++ front-end/test/Home.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() })
+
 import Home from '../src/Pages/Home.js'
 
 describe('<Home />', () => {
+  let home = shallow(<Home />)
 
   it('renders without crashing', () => {
-    // const div = document.createElement('div')
-    // ReactDOM.render(<Home />, div);
+    expect(home.find('h1').exists()).toBe(true);
   });
+
+  it('contains a heading', () => {
+    expect(home.find('h1').text()).toEqual("Donate, Period")
+  })
 
 });
